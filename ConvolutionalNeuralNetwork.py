8f59ccbc767797b39241721ca1e5e509c33e0a6d
vitocuccovillo <vito.cuccovillo@gmail.com>
2018-06-29 21:08:03 +0200

    Split files feedforward and convolutional

--- ConvolutionalNeuralNetwork.py
+++ ConvolutionalNeuralNetwork.py
@@ -1,7 +1,10 @@
 import tensorflow as tf
 from matplotlib import pyplot as plt
 import numpy as np
+#import os
+#os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 
+tf.logging.set_verbosity(tf.logging.INFO)
 
 def one_hot(labels, num_classes):
     results = np.zeros(shape=(len(labels), num_classes), dtype=np.float32)
@@ -29,13 +32,26 @@ print(model.summary())
 (train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.mnist.load_data()
 print('shape training images ', train_images.shape)
 print('shape test images ', test_images.shape)
+i = np.random.randint(0, len(train_images))
+print('displaying image {} with class {}'.format(i, train_labels[i]))
+plt.imshow(train_images[i], cmap='gray')
+plt.show()
+
+g = tf.Graph()
+
+flatten_shape = np.prod(train_images.shape[1:])
+
+with g.as_default():
+    X = tf.placeholder(tf.float32, [None, flatten_shape], name='X')
+    y = tf.placeholder(tf.float32, [None, 10], name='y')
+
 train_images_reshaped = train_images.reshape((-1, 28, 28, 1))
 test_images_reshaped = test_images.reshape((-1, 28, 28, 1))
 
 train_labels_one_hot = one_hot(train_labels, 10)
 test_labels_one_hot = one_hot(test_labels, 10)
 
-history = model.fit(train_images_reshaped, train_labels_one_hot, validation_split=0.1, epochs=5, batch_size=64);
+history = model.fit(train_images_reshaped, train_labels_one_hot, validation_split=0.1, epochs=5, batch_size=64)
 
 acc = history.history['acc']
 val_acc = history.history['val_acc']
